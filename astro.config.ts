1ba9e766926124930e76a07edaaeba09877c1017
Greg Cooney <gregdcooney@gmail.com>
2025-04-16 15:03:35 +1000

    another

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
-import { SITE } from "./src/config";
+import { SITE } from "./src/config.js";
 
 // https://astro.build/config
 export default defineConfig({
